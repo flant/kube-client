74dd66e8b2397c60a54294c488850950eb8bab39
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-11-06 17:48:02 +0300

    add named
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- client/client.go
+++ client/client.go
@@ -39,7 +39,7 @@ const (
 // TODO: refactor with methods
 func New(logger *log.Logger) *Client {
 	return &Client{
-		logger: logger.With("operator.component", "KubernetesAPIClient"),
+		logger: logger.Named("kubernetes-api-client").With("operator.component", "KubernetesAPIClient"),
 	}
 }
 
@@ -132,7 +132,7 @@ func (c *Client) RestConfig() *rest.Config {
 
 func (c *Client) Init() error {
 	if c.logger == nil {
-		c.logger = log.NewLogger(log.Options{}).With("operator.component", "KubernetesAPIClient")
+		c.logger = log.NewLogger(log.Options{}).Named("kubernetes-api-client").With("operator.component", "KubernetesAPIClient")
 	}
 
 	var err error
